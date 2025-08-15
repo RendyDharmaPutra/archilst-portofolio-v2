a9fa8d54c48c6ccdf4edc4eb837cc6dd8cd208e9
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2025-08-16 01:28:12 +0700

    style: remove old color variables in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,38 +6,6 @@ export default {
   theme: {
     extend: {
       colors: {
-        bg: {
-          DEFAULT: "#F8FAFC",
-          dark: "#0D1117",
-        },
-        text: {
-          DEFAULT: "#1E293B",
-          dark: "#F9FAFB",
-          secondary: {
-            DEFAULT: "#64748B",
-            dark: "#F9FAFB",
-          },
-          muted: {
-            DEFAULT: "#94A3B8",
-            dark: "#F9FAFB",
-          },
-        },
-        accent: "#0080FF",
-        card: {
-          DEFAULT: "#ffffff",
-          dark: "#161B22",
-        },
-        header: {
-          DEFAULT: "#F8FAFC",
-          dark: "#0D1117",
-        },
-        footer: {
-          DEFAULT: "#E2E8F0",
-          dark: "#0A0E13",
-        },
-        // danger: {
-        //   DEFAULT: "#EF4444",
-        // },
         primary: {
           accent: {
             DEFAULT: "#0080FF", // Biru utama
