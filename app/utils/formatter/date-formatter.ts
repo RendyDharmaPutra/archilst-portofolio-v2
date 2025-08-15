96abb83a5ee7328e98ec423c0ab8fc55780a3b6d
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2025-08-15 14:56:45 +0700

    refactor: change data format from ID to EN locales

--- app/utils/formatter/date-formatter.ts
+++ app/utils/formatter/date-formatter.ts
@@ -1,5 +1,5 @@
 export const formatDate = (rawDate: string) => {
-  return new Date(rawDate).toLocaleDateString("id-ID", {
+  return new Date(rawDate).toLocaleDateString("en-EN", {
     day: "2-digit",
     month: "long",
     year: "numeric",
