bd1008ca7b7624ffb8f6e0b59e5e8e27960dc3e5
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2025-08-17 23:22:52 +0700

    refactor: change padding button on mobile viewport

--- app/routes/projects/components/filter-tabs.tsx
+++ app/routes/projects/components/filter-tabs.tsx
@@ -16,7 +16,7 @@ export const FilterTabs = ({
           <button
             key={category}
             onClick={() => setActiveFilter(category)}
-            className={`px-6 py-3 font-medium rounded-md animate ${
+            className={`px-4 md:px-6 py-2 md:py-3 font-medium rounded-md animate ${
               activeFilter === category
                 ? "bg-primary-accent dark:bg-primary-accent-dark text-white shadow-[0_0_15px_rgba(0,128,255,0.3)] dark:shadow-[0_0_15px_rgba(0,102,204,0.3)]"
                 : "text-[#A1A1AA] hover:text-primary-accent dark:hover:text-primary-accent-dark hover:bg-primary-accent/10 dark:hover:bg-primary-accent-dark/10"
