a67d264f00cce632d925f62bb383685b314248fc
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2025-09-21 19:44:21 +0700

    feat: change the profile picture

--- app/routes/_index/components/hero/image.tsx
+++ app/routes/_index/components/hero/image.tsx
@@ -5,7 +5,7 @@ export const HeroImage = () => {
         <div className="absolute inset-0 bg-gradient-to-r from-primary-accent/20 via-primary-accent/5 to-primary-accent/20 dark:from-primary-accent-dark/30 dark:via-primary-accent-dark/10 dark:to-primary-accent-dark/30 rounded-full blur-xl animate-pulse animate"></div>
         {/* Profile Photo */}
         <img
-          src="/placeholder.svg?height=200&width=200"
+          src="https://uphgjpghsmzxwzkqtooq.supabase.co/storage/v1/object/public/profile/Profile%20Picture.jpg"
           alt="Profile Image"
           className={`relative w-48 h-48 md:w-56 md:h-56 object-cover rounded-full border-4 border-primary-accent/30 dark:border-primary-accent-dark/50 hover:border-primary-accent dark:hover:border-primary-accent-dark hover:shadow-[0_0_30px_rgba(0,128,255,0.4)] hover:scale-105 animate`}
         />
